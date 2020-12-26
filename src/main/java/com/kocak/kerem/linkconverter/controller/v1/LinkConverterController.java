6d0e3541303710d30cfc8c59706406b1b6e98ef3
Kocak, Kerem <kerem.kocak@accenture.com>
2020-12-26 13:32:08 +0200

    initial commit: running spring boot application with one dummy rest service running, spring security enabled, jacoco and swagger running, and mysql configuration specified.

--- src/main/java/com/kocak/kerem/linkconverter/controller/v1/LinkConverterController.java
+++ src/main/java/com/kocak/kerem/linkconverter/controller/v1/LinkConverterController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/api/v1/linkconverter")
-public class LinkCoverterController {
+public class LinkConverterController {
 
 }
